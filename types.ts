c42ad741a8ac16bb8c34bd6925ae6bb5e4a692e8
Erik Gardea <erik1gardea@gmail.com>
2019-11-02 11:45:04 -0500

    first commit, static types

---
+++ types.ts
@@ -0,0 +1,41 @@
+//Variable static types
+let myString: string;
+myString = 'hi';
+console.log(myString);
+
+let myNum: number;
+myNum = 23;
+console.log(myNum);
+
+let myBoolean: boolean;
+myBoolean = true;
+console.log(myBoolean);
+
+let myVar: any;
+myVar = 5;
+console.log(myVar);
+
+let strArray: string[];
+strArray = ['erik', 'gardea'];
+console.log(strArray);
+
+let numArray: number[];
+numArray = [1, 2, 3];
+console.log(numArray);
+
+let boolArray: boolean[];
+boolArray = [true, false, true];
+console.log(boolArray);
+
+let strNumTuple: [string, number];
+strNumTuple = ['erik', 26];
+console.log(strNumTuple);
+
+let myVoid: void = undefined;
+console.log(myVoid);
+
+let myNull: undefined = null;
+console.log(myNull);
+
+let myUndefined: undefined = null;
+console.log(myUndefined);
