d496feea9a46714612ee55fdd1918cb2df1e87b8
Erik Gardea <erik1gardea@gmail.com>
2019-11-02 11:55:22 -0500

    functions

---
+++ functions.js
@@ -0,0 +1,8 @@
+function getSome(num1, num2) {
+    return num1 + num2;
+}
+console.log(getSome(1, 2));
+var mySum = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(mySum(1, "2"));
---
+++ functions.ts
@@ -0,0 +1,9 @@
+function getSome (num1: number, num2: number):number{
+    return num1 + num2;
+}
+console.log(getSome(1, 2));
+
+let mySum = function(num1: number, num2: string) {
+    return num1 + num2;
+}
+console.log(mySum(1, "2"));
