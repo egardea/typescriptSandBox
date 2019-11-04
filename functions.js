6b43630d9b59544ef2aa66cd8269790a9c2e731b
egardea <erik1gardea@gmail.com>
2019-11-04 11:50:36 -0600

    functions

--- functions.js
+++ functions.js
@@ -3,6 +3,20 @@ function getSome(num1, num2) {
 }
 console.log(getSome(1, 2));
 var mySum = function (num1, num2) {
+    if (typeof num1 == 'string') {
+        num1 = parseInt(num1);
+    }
+    if (typeof num2 == 'string') {
+        num2 = parseInt(num2);
+    }
     return num1 + num2;
 };
 console.log(mySum(1, "2"));
+function getName(firstName, lastName) {
+    return firstName + ' ' + lastName;
+}
+console.log(getName('erik', 'gardea'));
+function myVoid() {
+    return 3;
+}
+console.log(myVoid());
--- functions.ts
+++ functions.ts
@@ -4,6 +4,23 @@ function getSome (num1: number, num2: number):number{
 console.log(getSome(1, 2));
 
 let mySum = function(num1: number, num2: string) {
+    if(typeof num1 == 'string') {
+        num1 = parseInt(num1);
+    } 
+    if(typeof num2 == 'string') {
+        num2 = parseInt(num2);
+    }
+
     return num1 + num2;
 }
 console.log(mySum(1, "2"));
+
+function getName(firstName:string, lastName:string):string{
+    return firstName+' '+lastName;
+}
+console.log(getName('erik', 'gardea'))
+
+function myVoid():void{
+    return 3;
+}
+console.log(myVoid());
