07bd3ffc11fae7d7c99b7cec1d4f0bf6d80a2d48
Erik Gardea <erik1gardea@gmail.com>
2019-11-06 18:43:46 -0600

    classes

--- interfaces.js
+++ interfaces.js
@@ -3,3 +3,8 @@ function showTodo(todo) {
 }
 var myTodo = { title: 'trash', text: 'take out trash' };
 showTodo(myTodo);
+function showTodo2(todo) {
+    console.log(todo.title + " " + todo.text);
+}
+var myTodo2 = { title: 'Trash', text: 'Take out trash' };
+showTodo2(myTodo2);
--- interfaces.ts
+++ interfaces.ts
@@ -3,3 +3,14 @@ function showTodo(todo: {title: string, text: string}){
 }
 let myTodo = {title: 'trash', text: 'take out trash'};
 showTodo(myTodo);
+
+interface Todo{
+    title: String;
+    text: String;
+}
+function showTodo2 (todo: Todo){
+    console.log(todo.title+" "+todo.text);
+}
+let myTodo2 = {title: 'Trash', text: 'Take out trash'}
+showTodo2(myTodo2);
+
